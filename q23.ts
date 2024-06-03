911be2c8de134880f2237b59a17c7f1b27cd733f
faazlasiddiqui <160077722+faazlasiddiqui@users.noreply.github.com>
2024-06-04 01:02:05 +0500

    add via upload

---
+++ q23.ts
@@ -0,0 +1,54 @@
+/* Conditional Tests: Write a series of conditional tests.
+ Print a statement describing each test and your prediction for the results of each test. 
+ Your code should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+• Look closely at your results, 
+and make sure you understand why each line evaluates to True or False.
+• Create at least 10 tests. 
+Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+*/
+
+//Making a variables
+let two:number = 2;
+let twenty:number = 20;
+
+//Test 1
+console.log("Test 1: two is equal to 2?");
+console.log(two == 2);
+//Test 2
+console.log("\nTest 2: twenty is equal to 20?");
+console.log(twenty == 20);
+
+//Test 3
+console.log("\nTest 3: two is not equal to 20?");
+console.log(two != twenty);
+
+//Test 4
+console.log("\nTest 4: 20 is greater then 2");
+console.log(twenty > two);
+
+//Test 5
+console.log("\nTest 5: 2 is smaller than 20");
+console.log(two < twenty);
+
+//Test 6
+console.log("\nTest 6: 20 is smaller than 2");
+console.log(twenty < two);
+
+//Test 7
+console.log("\nTest 7: 2 is equal to 20");
+console.log(two == twenty);
+
+//Test 8
+console.log("\nTest 8:20 is not equal to 20");
+console.log(two != 20);
+
+//Test 9
+console.log("\nTest 9: 2 is greater than 20");
+console.log(5 < two);
+
+//Test 10
+console.log("\nTest 10:500 is smaller than 20");
+console.log(500 < 20);
